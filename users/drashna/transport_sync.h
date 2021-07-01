9132cfa9f630e9b0fe25bf7aad3b8bcf985a3ef7
Drashna Jaelre <drashna@live.com>
2021-07-01 00:42:32 -0700

    Drashna's split updates (#13350)
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- users/drashna/transport_sync.h
+++ users/drashna/transport_sync.h
@@ -1,3 +1,4 @@
+
 /* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -15,32 +16,7 @@
  */
 
 #pragma once
+
 #include "drashna.h"
 
-// define diablo macro timer variables
-extern uint8_t diablo_times[];
-typedef struct {
-    uint16_t timer;
-    uint8_t  key_interval;
-    uint8_t  keycode;
-} diablo_timer_t;
-
-typedef struct {
-    uint8_t index;
-    uint8_t keycode;
-} diable_keys_t;
-
-extern diablo_timer_t diablo_timer[];
-
-void run_diablo_macro_check(void);
-
-#ifdef TAP_DANCE_ENABLE
-// clang-format off
-enum {
-    TD_D3_1 = 0,
-    TD_D3_2,
-    TD_D3_3,
-    TD_D3_4
-};
-// clang-format on
-#endif // TAP_DANCE_ENABLE
+void keyboard_post_init_transport_sync(void);
